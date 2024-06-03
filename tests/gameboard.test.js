1779e71e36bb164f3836c914b5803610a50a4297
Krissny <110375406+Krissny@users.noreply.github.com>
2024-06-03 15:59:15 +0530

    Add more tests for gameboard

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -7,3 +7,53 @@ test("constructor works properly", () => {
   expect(gameBoard.s3.length).toBe(3);
   expect(gameBoard.s4.length).toBe(2);
 });
+
+test("Receive attack works", () => {
+  const gameBoard = new GameBoard();
+  gameBoard.board[0][0] = 2; // there is a ship
+  const coord = { x: 0, y: 0 };
+  expect(gameBoard.receiveAttack(coord)).toBe(true);
+  expect(gameBoard.s2.hitCount).toBe(1);
+});
+
+test("Check invalid coordinates", () => {
+  const gameBoard = new GameBoard();
+  const coord = { x: -1, y: 2 };
+  expect(gameBoard.outOfBound(coord)).toBe(true);
+});
+test("Check valid coordinates", () => {
+  const gameBoard = new GameBoard();
+  const coord = { x: 0, y: 0 };
+  expect(gameBoard.outOfBound(coord)).toBe(false);
+});
+
+test("placeShips works correctly with valid coordinates", () => {
+  const gameBoard = new GameBoard();
+  const place = {
+    start: {
+      x: 0,
+      y: 0,
+    },
+    end: {
+      x: 5,
+      y: 0,
+    },
+  };
+  expect(gameBoard.placeShips(place.start, place.end, gameBoard.s1)).toBe(true);
+});
+test("placeShips works correctly with invalid coordinates", () => {
+  const gameBoard = new GameBoard();
+  const place = {
+    start: {
+      x: -1,
+      y: 0,
+    },
+    end: {
+      x: 5,
+      y: 0,
+    },
+  };
+  expect(gameBoard.placeShips(place.start, place.end, gameBoard.s1)).toBe(
+    false
+  );
+});
