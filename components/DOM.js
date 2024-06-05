597ea726bc90af43c1b4f5c07cd50112ca213283
Krissny <110375406+Krissny@users.noreply.github.com>
2024-06-05 13:17:44 +0530

    Add more functions

--- components/DOM.js
+++ components/DOM.js
@@ -1,4 +1,4 @@
-export function creategrid(Player) {
+export function creategrid(Player, turn) {
   const div = document.createElement("div");
   div.classList.add("gameboard");
   for (let i = 0; i < 10; i++) {
@@ -7,23 +7,34 @@ export function creategrid(Player) {
     for (let j = 0; j < 10; j++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
-      cell.dataset.row = i;
-      cell.dataset.col = j;
-      cell.addEventListener("click", reaction(Event, Player));
+      cell.setAttribute("data-row", i);
+      cell.setAttribute("data-col", j);
+      if (Player.name != "Computer" && Player.gameboard.board[i][j] > 0) {
+        cell.style.background = "green";
+      }
+      if (typeof Player.gameboard.board[i][j] != "number") {
+        cell.textContent = Player.gameboard.board[i][j];
+        cell.classList.add("disabled");
+      } else if (Player.gameboard.board[i][j] < 0) {
+        cell.style.background = "red";
+        cell.classList.add("disabled");
+      }
+      cell.addEventListener("click", (Event) => {
+        let x = Event.currentTarget.dataset.row;
+        let y = Event.currentTarget.dataset.col;
+        console.log(x, y);
+        Player.attacked(x, y);
+        if (typeof Player.gameboard.board[x][y] == "number") {
+          Event.target.style.background = "red";
+        } else {
+          Event.target.textContent = Player.gameboard.board[i][j];
+        }
+        cell.classList.add("disabled");
+        turn();
+      });
       rowDiv.appendChild(cell);
     }
     div.appendChild(rowDiv);
   }
   return div;
-}
-
-function reaction(Event, Player) {
-  let x = Event.target.dataset.row;
-  let y = Event.target.dataset.row;
-  Player.attacked(x, y);
-  if (typeof Player.gameboard.board[x][y] == "number") {
-    Event.target.style.backgound = "red";
-  } else {
-    Event.target.textContent = ".";
-  }
 }
--- main.js
+++ main.js
@@ -1,4 +1,75 @@
-import creategrid from "./components/DOM";
+import { creategrid } from "./components/DOM.js";
+import { Player } from "./components/player.js";
 
-// console.log(creategrid);
-console.log("it works");
+let player1 = new Player("Krishna");
+let player2 = new Player("Computer");
+let flag = 1;
+
+const div1 = document.querySelector(".player1");
+const div2 = document.querySelector(".player2");
+const res = document.querySelector(".reset");
+const tip = document.querySelector(".tip");
+
+let gameboard1 = creategrid(player1, turn);
+let gameboard2 = creategrid(player2, turn);
+
+div1.appendChild(gameboard1);
+div2.appendChild(gameboard2);
+console.log(player2.gameboard.board);
+
+function turn() {
+  console.log(player2.gameboard.board);
+  setTimeout(checkWinner, 300);
+  if (flag === 1) {
+    gameboard1.classList.add("disabled");
+    gameboard2.classList.remove("disabled");
+    flag = 2;
+  } else {
+    tip.textContent = "";
+    gameboard1.classList.add("disabled");
+    gameboard2.classList.remove("disabled");
+    const coord = player2.randomAttack();
+    console.log(coord);
+    player1.attacked(coord.x, coord.y);
+    flag = 1;
+    gameboard1 = creategrid(player1, turn);
+    div1.removeChild(div1.firstChild);
+    div1.appendChild(gameboard1);
+
+    gameboard2 = creategrid(player2, turn);
+    div2.removeChild(div2.firstChild);
+    div2.appendChild(gameboard2);
+    // catchCell();
+    // checkWinner();
+    turn();
+  }
+}
+turn();
+
+function reset() {
+  player1 = new Player("Krishna");
+  player2 = new Player("Computer");
+  flag = 1;
+
+  gameboard1 = creategrid(player1, turn);
+  gameboard2 = creategrid(player2, turn);
+
+  div1.removeChild(div1.firstChild);
+  div2.removeChild(div2.firstChild);
+
+  div1.appendChild(gameboard1);
+  div2.appendChild(gameboard2);
+  turn();
+}
+
+res.addEventListener("click", reset);
+
+function checkWinner() {
+  if (player1.gameboard.shipsSunk()) {
+    alert("You Lost");
+    reset();
+  } else if (player2.gameboard.shipsSunk()) {
+    alert("Congratulations! You Won");
+    reset();
+  }
+}
